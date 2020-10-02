174b2cd62b9c2032c2110d1d6753b36f93dd4d6e
strelec-i <il.strelcov@gmail.com>
2020-10-02 12:55:59 +0300

    feat: solution typical-array-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((acc, cur) => acc + cur, 0) / array.length;
+    }
+};
